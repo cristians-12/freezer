98b3d80e3293ca5ca56c074714b14f36748d459f
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-11-22 00:12:37 -0500

    add update user

---
+++ backend/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('/user/{id}', 'getUser');
+    Route::post('/user', 'createUser');
+});
